53552621a39f5dee6c414f9f15977bc04fd3663b
admin <admin@TeamCity>
2021-06-02 10:30:34 +0200

    TeamCity change in 'Command Line' project: runners of 'run script from working directory (custom script)' build configuration were updated

--- .teamcity/CommandLine/buildTypes/CommandLine_RunScriptFromWorkingDirectoryCustomScript.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_RunScriptFromWorkingDirectoryCustomScript.kt
@@ -1,7 +1,7 @@
 package CommandLine.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.exec
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object CommandLine_RunScriptFromWorkingDirectoryCustomScript : BuildType({
     uuid = "67b8d113-4768-48bf-bf94-4a7b281b1a4c"
@@ -12,9 +12,11 @@ object CommandLine_RunScriptFromWorkingDirectoryCustomScript : BuildType({
     }
 
     steps {
-        exec {
-            workingDir = "subdirectory"
-            path = "subdirectory/hello_world.sh"
+        script {
+            scriptContent = "subdirectory/hello_world.sh"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
         }
     }
 })
