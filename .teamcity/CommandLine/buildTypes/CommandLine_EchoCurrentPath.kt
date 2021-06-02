3988378a8fb05853b04bba3f6404aaee665e955b
admin <admin@TeamCity>
2021-06-02 11:02:31 +0200

    TeamCity change in 'Command Line' project: runners of 'Echo current path' build configuration were updated

--- .teamcity/CommandLine/buildTypes/CommandLine_EchoCurrentPath.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_EchoCurrentPath.kt
@@ -1,8 +1,21 @@
 package CommandLine.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object CommandLine_EchoCurrentPath : BuildType({
     uuid = "2dbaaca0-cacd-4a07-ae00-fab6ee03f12f"
     name = "Echo current path"
+
+    steps {
+        script {
+            scriptContent = """
+                echo "current path:"
+                cd
+            """.trimIndent()
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
 })
