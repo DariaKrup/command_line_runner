4a52184a2352d685d4238e289b467f7c1dae6011
admin <admin@TeamCity>
2021-06-07 14:06:06 +0200

    TeamCity change in 'Command Line' project: build step was copied in 'build timeout + endless build' build configuration

--- .teamcity/CommandLine/buildTypes/CommandLine_BuildTimeoutEndlessBuild.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_BuildTimeoutEndlessBuild.kt
@@ -28,5 +28,12 @@ object CommandLine_BuildTimeoutEndlessBuild : BuildType({
             param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
             param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
         }
+        script {
+            name = "New build step (1) (1)"
+            scriptContent = "ping google.com"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
     }
 })
