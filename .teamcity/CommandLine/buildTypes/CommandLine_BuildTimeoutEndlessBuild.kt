a816bb9103ff7325f466eb9330dd6abeea6bcb2a
admin <admin@TeamCity>
2021-06-07 14:00:27 +0200

    TeamCity change in 'Command Line' project: runners of 'build timeout + endless build' build configuration were updated

--- .teamcity/CommandLine/buildTypes/CommandLine_BuildTimeoutEndlessBuild.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_BuildTimeoutEndlessBuild.kt
@@ -1,8 +1,18 @@
 package CommandLine.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object CommandLine_BuildTimeoutEndlessBuild : BuildType({
     uuid = "02df5fe7-4f6f-4964-b85a-6210394dd03e"
     name = "build timeout + endless build"
+
+    steps {
+        script {
+            scriptContent = "ping google.com"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
 })
