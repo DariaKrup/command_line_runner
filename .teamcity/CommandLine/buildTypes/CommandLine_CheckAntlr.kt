7b964abd243f7b1679c70a5a069a6082adbf622a
admin <admin@TeamCity>
2022-10-05 16:10:11 +0000

    TeamCity change in 'Command Line' project: parameters of 'check antlr' build configuration were updated

--- .teamcity/CommandLine/buildTypes/CommandLine_CheckAntlr.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_CheckAntlr.kt
@@ -7,6 +7,10 @@ object CommandLine_CheckAntlr : BuildType({
     uuid = "06778d6e-f9d8-43d7-a8f9-8b168ef22292"
     name = "check antlr"
 
+    params {
+        param("antlr", "system.antlr")
+    }
+
     steps {
         script {
             scriptContent = "%antlr%"
