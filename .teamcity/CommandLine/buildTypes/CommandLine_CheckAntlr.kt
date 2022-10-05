66c8f8db8a5336660de4eae6586b5988dced7e92
admin <admin@TeamCity>
2022-10-05 16:09:13 +0000

    TeamCity change in 'Command Line' project: runners of 'check antlr' build configuration were updated

--- .teamcity/CommandLine/buildTypes/CommandLine_CheckAntlr.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_CheckAntlr.kt
@@ -1,8 +1,18 @@
 package CommandLine.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object CommandLine_CheckAntlr : BuildType({
     uuid = "06778d6e-f9d8-43d7-a8f9-8b168ef22292"
     name = "check antlr"
+
+    steps {
+        script {
+            scriptContent = "antlr"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
 })
