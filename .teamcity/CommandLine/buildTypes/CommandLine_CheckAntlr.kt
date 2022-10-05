2a4c2a9c76db06b481e2bafacf80ccb5083eb740
admin <admin@TeamCity>
2022-10-05 16:08:36 +0000

    TeamCity change in 'Command Line' project: 'check antlr' build configuration was created

---
+++ .teamcity/CommandLine/buildTypes/CommandLine_CheckAntlr.kt
@@ -0,0 +1,8 @@
+package CommandLine.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+
+object CommandLine_CheckAntlr : BuildType({
+    uuid = "06778d6e-f9d8-43d7-a8f9-8b168ef22292"
+    name = "check antlr"
+})
