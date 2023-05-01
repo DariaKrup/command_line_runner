e8d999de28c564cb31a629cfc1b77e377290d2da
admin <admin@TeamCity>
2023-05-01 18:18:08 +0200

    TeamCity change in 'Command Line' project: runners of 'ls content of the home directory' build configuration were updated

--- .teamcity/CommandLine/buildTypes/CommandLine_LsContentOfTheHomeDirectory.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_LsContentOfTheHomeDirectory.kt
@@ -1,8 +1,18 @@
 package CommandLine.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object CommandLine_LsContentOfTheHomeDirectory : BuildType({
     uuid = "b15d5ba7-e0b4-4026-ab31-f2de7853fa8c"
     name = "ls content of the home directory"
+
+    steps {
+        script {
+            scriptContent = "ls -la ~"
+            param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
+            param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
+            param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
 })
