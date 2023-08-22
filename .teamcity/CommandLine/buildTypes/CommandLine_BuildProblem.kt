650bddb89f06a6e136b54f519dd8030f8aa5d7cf
admin <admin@TeamCity>
2023-08-22 10:12:03 +0200

    TeamCity change in 'Command Line' project: build features of 'build problem' build configuration were updated

--- .teamcity/CommandLine/buildTypes/CommandLine_BuildProblem.kt
+++ .teamcity/CommandLine/buildTypes/CommandLine_BuildProblem.kt
@@ -1,6 +1,7 @@
 package CommandLine.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object CommandLine_BuildProblem : BuildType({
@@ -16,6 +17,11 @@ object CommandLine_BuildProblem : BuildType({
             scriptContent = """echo "##teamcity[buildStatus status='FAILURE' text='xxxhad been pushed successfully']""""
             param("teamcity.runAs.windowsIntegrityLlevel", "auto")
             param("teamcity.runAs.loggingLevel", "off")
+        }
+    }
+
+    features {
+        perfmon {
         }
     }
 })
